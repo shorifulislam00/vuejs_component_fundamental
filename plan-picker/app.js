5bace2cbe5f437ae77c7f3d55f8bc20003c0a04d
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-02 18:37:26 +0600

    nested component

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,11 +1,17 @@
+Vue.component('plan-picker', {
+    template: '#plan-picker-template',
+    data (){
+        return {
+            plans: ['The hacker', 'The single', 'The Curious', 'The Addict']
+        }
+    }
+})
+
 Vue.component('plan', {
     template: '#plan-template',
     props: ['name']
 })
 
 new Vue({
-    el: "#app",
-    data: {
-        plans: ['The hacker', 'The single', 'The Curious', 'The Addict']
-    }
+    el: "#app"
 })
