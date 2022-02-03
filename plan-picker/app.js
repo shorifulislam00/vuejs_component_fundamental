405d3d0245757dee63422988ba5ff2ba20c513b1
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-03 16:09:00 +0600

    communication with component and event

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,17 +1,50 @@
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    data (){
-        return {
-            plans: ['The hacker', 'The single', 'The Curious', 'The Addict']
+let planComponent = {
+    template: '#plan-template',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        selectedPlan: {
+            type: String
+        }
+    },
+    computed: {
+        isSelected(){
+            return this.name === this.selectedPlan
+        }
+    },
+    methods: {
+        select(){
+            this.$emit('select', this.name)
+            // this.selected = true
         }
     }
-})
+}
 
-Vue.component('plan', {
-    template: '#plan-template',
-    props: ['name']
-})
+let planPickerComponent = {
+    template: '#plan-picker-template',
+    components: {
+        plan: planComponent
+    },
+    data (){
+        return {
+            plans: ['The hacker', 'The single', 'The Curious', 'The Addict'],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan){
+            this.selectedPlan = plan
+        }
+    }
+}
+
+
 
 new Vue({
-    el: "#app"
+    el: "#app",
+    components: {
+        planPicker: planPickerComponent
+    }
 })
