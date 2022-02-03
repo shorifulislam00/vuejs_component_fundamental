04b288d5913fdf63e5cfd7fc8937f42933e4a158
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-03 21:30:22 +0600

    notification message system with vue

---
+++ notification/app.js
@@ -0,0 +1,27 @@
+Vue.component('notificationMessage', {
+    template: "#notification-message-template",
+    props: {
+        type: {
+            type: String,
+            default: 'info'
+        },
+        header: {
+            type: String,
+            default: 'Information'
+        }
+    },
+    data () {
+        return {
+            hidden: false
+        }
+    },
+    methods: {
+        hide() {
+            this.hidden = true
+        }
+    }
+})
+
+new Vue({
+    el: "#app"
+})
